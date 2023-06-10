fb73d7befd7142b8d9ecc9d3fdf5206f9fbf6722
262094 <92551139+262094@users.noreply.github.com>
2023-06-10 22:33:52 +0200

    Add files via upload

---
+++ financify.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Financify Application</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      font-size: 18px;
+      text-align: center;
+      background-image: url('background_graph1.jpg');
+      background-size: 50%;
+      background-position: center;
+      background-repeat: no-repeat;
+    }
+    h1 {
+      font-size: 40px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    h2{
+      font-size: 30px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    h3{
+      font-size: 50px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    li{
+         font-size: 24px;
+
+    }
+    p {
+      margin-bottom: 10px;
+      font-size: 24px;
+    }
+  </style>
+</head>
+<body>
+  <h1><?php echo "What is the Financify application and why is it worth using?"; ?></h1>
+  <p><?php echo "Have you ever wondered how expenses affect your finances? Do you know that controlling expenses can bring numerous benefits to you and your future savings? Analyzing expenses is becoming increasingly popular and positively impacting the resources of your wallet. In today's times, when inflation keeps rising, more and more people pay attention to their expenses. In this article, you will learn why it is worth saving and how the Financify application can help you in this regard. With our application, we will help you control your budget."; ?></p>
+
+  <h2><?php echo "What is saving?"; ?></h2>
+  <p><?php echo "Saving is one of the most important financial aspects because it allows you to ensure a stable financial future and protection against unexpected expenses or sudden changes in life situations. Saving can be done in many different ways, including opening a savings account at a bank, investing in investment funds or real estate, or simply by limiting expenses and planning a household budget as is the case with Financify. It is important to understand different saving methods and their application depending on individual financial needs and goals."; ?></p>
+
+  <p><?php echo "The process of saving can involve many actions, such as:"; ?></p>
+  
+  
+    <li><?php echo "Expense reduction - consciously reducing expenses on unnecessary things such as take-out food, coffee at cafes, unnecessary items, etc,"; ?></li>
+    <li><?php echo "Planning a household budget - determining fixed expenses such as rent, food, transportation, and others, as well as identifying savings opportunities by reducing unnecessary expenses,"; ?></li>
+    <li><?php echo "Detailed expense monitoring and analysis - ongoing expense monitoring allows for better planning of future investments; a list of data in the form of expense and savings charts gives us a wider range of possibilities to manage our own finances."; ?></li>
+  
+
+  <p><?php echo "These and many other actions can help in the saving process and increase financial stability. It is crucial to understand your needs and goals in order to choose appropriate savings actions. To implement specific financial solutions, our Financify application offers an easy and convenient way to start saving. Simply register in our application and begin the saving process. After registering, you gain access to your own account where you will have the ability to:"; ?></p>
+  
+  
+    <li><?php echo "Plan future expenses,"; ?></li>
+    <li><?php echo "Access budget charts,"; ?></li>
+    <li><?php echo "Achieve your savings goals,"; ?></li>
+    <li><?php echo "Receive tips on smart saving,"; ?></li>
+    <li><?php echo "Use a user-friendly interface,"; ?></li>
+    <li><?php echo "Analyze your current expenses."; ?></li>
+  
+
+  <h2><?php echo "How will saving with Financify impact your budget?"; ?></h2>
+  <p><?php echo "By using our application, you can help your expenses and increase the efficiency of your savings, which will positively impact your budget. By using Financify, you enhance your efficiency in managing expenses. Join our platform and promote the idea of saving together with us. Create better expenses with us today! Saving has never been easier!"; ?></p>
+  <h3><?php echo "Start saving money with Financify today"; ?></h3>
+</body>
+</html>
